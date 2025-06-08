00e0fe085dd48c5252618d2c1ab9507eb72b8e99
juancho391 <juanchischis2010@hotmail.com>
2025-06-08 13:59:12 -0500

    creacion de la tabla usuarios en la bd

---
+++ db/postgresql/entities.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
+
+
+class Users(SQLModel, table=True):
+    __tablename__= "users"
+
+    id : int | None = Field(default=None, primary_key=True)
+    name : str 
+    cedula : str 
+    password : str
+    email : EmailStr
+    edad : int 
