314878d8eddf7ac7fd7f060e09ba5b1ba7d73828
juancho391 <juanchischis2010@hotmail.com>
2025-06-09 20:14:21 -0500

    router para las reservas

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from .users.router import users_router
+from .reservas.router import booking_router
 
 
 def register_routers(app: FastAPI):
     app.include_router(users_router, prefix="/users")
+    app.include_router(booking_router, prefix="/reservas")
