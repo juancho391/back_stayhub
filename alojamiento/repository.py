2e558c59709ed4928507b3ad2c797d58a5ed93d1
Jhos10 <jhostinposada7@gmail.com>
2025-06-10 07:47:09 -0500

    Se hizo el repository de alojamiento

---
+++ alojamiento/repository.py
@@ -0,0 +1,17 @@
+from ..db.mongodb.conexion import mongo_db_dependency
+from typing import Annotated
+from fastapi import Depends
+class LodgingRepository:
+    def __init__(self, mongodb: mongo_db_dependency):
+        self.bd = mongodb
+        self.collection = self.bd['alojamiento']
+
+    def get_lodgins(self):
+        lodgins = self.collection.find({})
+        return lodgins
+    
+
+def get_lodging_repository(mongodb:mongo_db_dependency):
+    return LodgingRepository(mongodb=mongodb)
+
+lodging_repository_dependency = Annotated[LodgingRepository,Depends(get_lodging_repository)]
