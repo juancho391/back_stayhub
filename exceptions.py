f9c32dc3199a924087ad7ff26f4c71630dc3bcf4
juancho391 <juanchischis2010@hotmail.com>
2025-06-09 20:16:17 -0500

    exception para manejar el error cuando hay traslape en las fechas de una nueva reserva

--- exceptions.py
+++ exceptions.py
@@ -1,19 +1,28 @@
 from fastapi import HTTPException
 
+
 class UserError(HTTPException):
     pass
 
 
 class UserLoginError(UserError):
-    def __init__(self, status_code, detail = None):
+    def __init__(self, status_code, detail=None):
         super().__init__(status_code, detail)
+
 
 class UserRegisterError(UserError):
     def __init__(self, message: str = "Ya existe un usuario registrado con este email"):
-        super().__init__(status_code = 401, detail=message)
+        super().__init__(status_code=401, detail=message)
+
 
 # Excepcion de autenticacion de usuario
 class AuthenticationError(HTTPException):
     def __init__(self, message: str = "No se pudo validar el usuario"):
         super().__init__(status_code=401, detail=message)
 
+
+class BookingError(HTTPException):
+    def __init__(
+        self, status_code=400, detail="No se puede realizar la reserva en esas fechas"
+    ):
+        super().__init__(status_code, detail)
