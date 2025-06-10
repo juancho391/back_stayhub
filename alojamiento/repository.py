fbdd66b1c4e349d75de2ada40330ff5fdf966fd6
juancho391 <juanchischis2010@hotmail.com>
2025-06-10 14:37:36 -0500

    formateo del codigo en el lodginrepository

--- alojamiento/repository.py
+++ alojamiento/repository.py
@@ -1,17 +1,22 @@
 from ..db.mongodb.conexion import mongo_db_dependency
 from typing import Annotated
 from fastapi import Depends
+
+
 class LodgingRepository:
     def __init__(self, mongodb: mongo_db_dependency):
         self.bd = mongodb
-        self.collection = self.bd['alojamiento']
+        self.collection = self.bd["alojamiento"]
 
     def get_lodgins(self, limit=10):
         lodgins = self.collection.find({}).limit(limit)
         return lodgins
-    
 
-def get_lodging_repository(mongodb:mongo_db_dependency):
+
+def get_lodging_repository(mongodb: mongo_db_dependency):
     return LodgingRepository(mongodb=mongodb)
 
-lodging_repository_dependency = Annotated[LodgingRepository,Depends(get_lodging_repository)]
+
+lodging_repository_dependency = Annotated[
+    LodgingRepository, Depends(get_lodging_repository)
+]
